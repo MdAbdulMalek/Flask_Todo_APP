5555ed244938d3ade35fe36e58358f4f989dc061
abdul.malek <gravityd000@gmail.com>
2023-11-08 18:20:23 +0600

    [ADD] new updated in plumber.

--- PDF_Plumber/app.py
+++ PDF_Plumber/app.py
@@ -29,7 +29,7 @@ def home():
         # List the vendors of interest here. Every individual here needs unique logic.
         # Currenlty, there are only 3 vendors we have configured.
         # Test ONLY the inovoices of the vendors mentioned here
-        v_list = ['Select One Vendor', 'Costco Canada', 'Acme Dry Ice', 'Edgewater Properties LLC']
+        v_list = ['Select One Vendor', 'Costco Canada', 'Acme Dry Ice', 'Edgewater Properties LLC', 'Cambrige']
         try:
             if selected_ven == "Costco Canada":
                 with pdfplumber.open(UPLOAD_FOLDER+"/"+file_path[0]) as pdf:
@@ -89,6 +89,74 @@ def home():
                     os.remove(path)
                 context = {'json_output_line':json_output_line, 'final_dic_other':final_dic_other, 'v_list':v_list}
                 return render_template("index.html", context=context)
+            elif selected_ven == "Cambrige":
+                 def extracting_miscellaneous_item(miscellaneous_list,result_dict):
+                    if 'Miscellaneous' not in result_dict:
+                        mis_item = {}
+                        mis_item[miscellaneous_list[0][0]] = miscellaneous_list[1][0]
+                        mis_item[miscellaneous_list[0][1]] = miscellaneous_list[1][1]
+                        mis_item[miscellaneous_list[0][2]] = miscellaneous_list[1][2]
+                        result_dict['Miscellaneous'] = mis_item
+                 def extract_item_information_from_Cambridge_Scientific(item_tables,result_dict):
+                        item_table = [[item for item in sublist if item is not None] for sublist in item_tables]
+                        miscellaneous_list = item_table[:2]
+                        result_dict = extracting_miscellaneous_item(miscellaneous_list,result_dict)
+                        filtered_list = item_table[2:-3]
+                        item_list = [item for item in filtered_list if any(subitem.strip() != '' for subitem in item)]
+                        df = pd.DataFrame(item_list[1:], columns=item_list[0])
+                        df_dict = df.to_dict(orient='records')
+                        if 'ITEM' not in result_dict:
+                            result_dict['ITEM'] = []
+                        result_dict['ITEM'].append(df_dict)
+                        return result_dict 
+                 def extract_invoice_number_information_Cambridge_Scientific(table,result_dict):
+                        df = pd.DataFrame(table[1:], columns=table[0])
+                        column_names = df.columns.tolist()
+                        df_dict = df.to_dict(orient='records')
+                        df_dict = [item for item in df_dict if item is not None]
+                        result_dict[column_names[0]] = df_dict[0][column_names[0]]
+                        result_dict[column_names[1]] = df_dict[0][column_names[1]]
+                        return result_dict 
+                 def extract_BillTo_ShipTo_from_Cambridge_Scientific(bill_table,ship_table,result_dict):
+                        result_dict[bill_table[0][0]] = bill_table[1][0]
+                        result_dict[ship_table[0][0]] = ship_table[1][0]
+                        return result_dict 
+                 def get_total_values_from_Cambridge_Scientific(total_tables,result_dict):
+                        total_table = [[item for item in sublist if item is not None] for sublist in total_tables]
+                        text = total_table[-1][0]
+                        new_text = re.sub(r'\(cid:\d+\)', '$', text)
+                        result_dict['Total'] = new_text
+                        return result_dict 
+                 def extract_invoice_table_from_Cambridge_Scientific(pdf_path):
+                        
+                        test_table_settings = {
+                            "horizontal_strategy":"lines",
+                            "vertical_strategy":"lines"
+                        }
+                        test_tables = []
+                        result_dict = {}
+                        with pdfplumber.open(pdf_path) as pdf:
+                            for page_number in range(len(pdf.pages)):
+                                page = pdf.pages[page_number]
+                                extracted_tables = page.extract_tables(test_table_settings)
+                                print("result_dictvvvvvvvvvvvvvvvv", extracted_tables)
+                                if page_number==0:
+                                    result_dict = extract_invoice_number_information_Cambridge_Scientific(extracted_tables[0],result_dict)
+                                    result_dict = extract_BillTo_ShipTo_from_Cambridge_Scientific(extracted_tables[1],extracted_tables[2],result_dict)
+                                if page_number==(len(pdf.pages)-1):
+                                    result_dict = get_total_values_from_Cambridge_Scientific(extracted_tables[-1],result_dict)
+                                    result_dict = extract_item_information_from_Cambridge_Scientific(extracted_tables[3],result_dict)
+                            print("result_dictvvvvvvvvvvvvvvvv", result_dict)
+                        return result_dict 
+                 def process_of_extracting_Cambridge_Scientific_template(pdf_path):
+                        result_dict = extract_invoice_table_from_Cambridge_Scientific (pdf_path)
+                        return result_dict
+                 pdf_path = UPLOAD_FOLDER+"/"+file_path[0]
+                 print("pdf_path",  pdf_path)
+                 results = process_of_extracting_Cambridge_Scientific_template(UPLOAD_FOLDER+"/"+file_path[0]) 
+                 print("results", results)
+                 context = {'json_output_line':results, 'final_dic_other':final_dic_other, 'v_list':v_list}
+                 return render_template("index.html", context=context)
             
             elif selected_ven == "Acme Dry Ice":
                 with pdfplumber.open(UPLOAD_FOLDER+"/"+file_path[0]) as pdf:
@@ -102,11 +170,8 @@ def home():
                 if data_frame['Description'].values[-1] != "Shipping Charges":
                     data_frame = data_frame.drop(data_frame.index[-1])
                 data_frame = data_frame.rename_axis('LINE')
-                # data_frame.columns = [column.upper() for column in data_frame.columns]
-                # print("data_frame", data_frame)
                 column_names = column_names.split(",")
                 if '' in column_names: column_names.remove('')
-                # print("column_names", column_names)
                 required_columns = list(map(lambda text: re.sub(r'\s+', ' ', text.strip()), column_names))
                 for col in required_columns:
                     if col not in list(data_frame.columns):
@@ -163,7 +228,7 @@ def home():
             context = {'json_output_line':[], 'final_dic_other':[], 'v_list':v_list}
             return render_template("index.html", context=context)
     else:
-        v_list = ['Select One Vendor', 'Costco Canada', 'Acme Dry Ice', 'Edgewater Properties LLC']
+        v_list = ['Select One Vendor', 'Costco Canada', 'Acme Dry Ice', 'Edgewater Properties LLC', 'Cambrige']
         context = {'json_output_line':[], 'final_dic_other':[], 'v_list':v_list}
         return render_template("index.html", context=context)
 
